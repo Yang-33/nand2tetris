cb459c992d61ec8ab7afe8c8934472146b215d0f
Yang-33 <kasai.yuta0810@gmail.com>
2020-03-07 22:34:44 +0900

    Simplify project's directories

--- run-tests.sh
+++ run-tests.sh
@@ -2,6 +2,6 @@
 
 find ./build -name "tests*" -type f -executable | xargs -n 1 -I {} sh -c '{} --gtest_list_tests'
 find ./build -name "tests*" -type f -executable | xargs -n 1 -I {} sh -c '{}'
-sh projects/01/run-test.sh
-sh projects/02/run-test.sh
+sh source/projects/01/run-test.sh
+sh source/projects/02/run-test.sh
 
