3f0f3d03cc34b1a56741b9479eaec8cb41f56315
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-03-07 16:53:42 +0900

    Add HardwareSimulator test script to CI (#6)

--- run-tests.sh
+++ run-tests.sh
@@ -2,3 +2,5 @@
 
 find ./build -name "tests*" -type f -executable | xargs -n 1 -I {} sh -c '{} --gtest_list_tests'
 find ./build -name "tests*" -type f -executable | xargs -n 1 -I {} sh -c '{}'
+sh projects/01/run-test.sh
+
