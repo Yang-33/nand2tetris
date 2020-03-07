ddc5e515fc2f7251d3b2fba5d3a64e4cd3b1440d
Yuta Kasai <kasai.yuta0810@gmail.com>
2020-03-07 22:26:35 +0900

    Add ALU.hdl (#7)

---
+++ projects/02/run-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+status=0
+for f in projects/02/*.tst;
+do
+  echo $f
+  sh tools/HardwareSimulator.sh $f;
+  if [ $? -ne 0 ]; then
+    status=1
+  fi
+done
+
+exit $status
--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,5 @@
 find ./build -name "tests*" -type f -executable | xargs -n 1 -I {} sh -c '{} --gtest_list_tests'
 find ./build -name "tests*" -type f -executable | xargs -n 1 -I {} sh -c '{}'
 sh projects/01/run-test.sh
+sh projects/02/run-test.sh
 
